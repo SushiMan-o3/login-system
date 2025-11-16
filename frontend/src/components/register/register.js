88f8e7048591ef042a94856f68e701f044169bc0
Sushim Malla <sushim.m17@gmail.com>
2025-11-15 21:09:11 -0500

    Tried making a basic register with js and css. Will implement once I am done

--- frontend/src/components/register/register.js
+++ frontend/src/components/register/register.js
@@ -6,9 +6,19 @@ export default function register() {
     return (
         <div className="App">
             <ParticlesBackground />
-            <div style={{ position: "relative", zIndex: 1, color: "white", marginTop: "4rem", textAlign: "center" }}>
-                <h1>Create</h1>
-                <p>Create your account here.</p>
+            <div className="name">
+                <h1>Create Account</h1>
+                
+                <p>name</p>
+                <input type="text" id="name" name="name" className="inputBox" required />
+
+                <p>email</p>
+                <input type="email" id="email" name="email" className="inputBox" required />
+
+                <p>password</p>
+                <input type="password" id="password" name="password" className="inputBox" required />
+
+                <button type="submit" className="signupbutton">Sign Up</button>
             </div>
         </div>
     );
