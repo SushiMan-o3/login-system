b0ef0f495404de9889db4b282541428ee1d130fa
Sushim Malla <sushim.m17@gmail.com>
2025-12-01 03:27:04 -0500

    Cleaned everything out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,6 @@ import Home from "./components/home/home";
 import Login from "./components/home/login/Login";
 import Register from "./components/register/Register"; 
 
-
-let loggedIn = false;
-
-const userInfo = {
-  
-};
-
 function App() {
   return (
     <div className="App">
